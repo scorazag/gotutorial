edc4b15fb4cdf36e7cc693cc1abc4236be62aae3
sergo coraza <scorazago@gmail.con>
2019-08-13 20:23:58 -0500

    metod post

--- src/main/maing.go
+++ src/main/maing.go
@@ -3,8 +3,21 @@ package main
 import(
 	"fmt"
 	"net/http"
+	"io/ioutil"
+	"log"
+	"encoding/json"
 	"github.com/labstack/echo"
 )
+
+type Cat struct {
+	Name string "json:name"
+	Type string "json:type"
+}
+
+type Dog struct {
+	Name string "json:name"
+	Type string "json:type"
+}
 
 func yallo(c echo.Context) error{
 	return c.String(http.StatusOK,"Hola  desde server")
@@ -29,12 +42,54 @@ func getCats(c echo.Context) error{
 		"error":"salio mal chavo",
 	})
 }
+
+func addCat(c echo.Context) error {
+	cat := Cat{}
+
+	defer c.Request().Body.Close()
+
+	b, err := ioutil.ReadAll(c.Request().Body)
+
+	if err != nil {
+		log.Println("Fallo al leer")
+		return c.String(http.StatusInternalServerError,"")
+	}
+
+	err = json.Unmarshal(b, &cat)
+
+	if err != nil {
+		log.Println("Fallo ")
+		return c.String(http.StatusInternalServerError,"")
+	}
+
+	log.Println(cat)
+	return c.String(http.StatusOK, "Gatitot agregado")
+
+}
+
+func addDog(c echo.Context) error{
+	dog := Dog{}
+
+	defer c.Request().Body.Close()
+
+	err := json.NewDecoder(c.Request().Body).Decode(&dog)
+
+	if err != nil {
+		log.Println("Proceso fallido %s",err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Println("Perrito %#v",dog)
+	return c.String(http.StatusOK, "Perrito agregado")
+}
 func main(){
 	fmt.Println("Hello")
 
 	e := echo.New()
 	e.GET("/",yallo)
 	e.GET("/cats/:data",getCats)
+	e.POST("/cats", addCat)
+	e.POST("/dogs",addDog)
 
 	//start the server
 	e.Start(":8000")
